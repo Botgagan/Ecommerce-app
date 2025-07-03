4e6c8a5ca383488a4b5b4c3ce9d9e61f8f4cf69f
Gagandeep Singh <gagan112002@gmail.com>
2025-07-04 03:25:32 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import Home from './pages/home'
-import Collection from './pages/collection'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
@@ -11,7 +11,7 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import SearchBar from './components/searchBar'
+import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'keen-slider/keen-slider.min.css';
